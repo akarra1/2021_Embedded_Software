c492cdff4fee3868fc559924f9b4d7e0fe7c19dd
akettle44 <andrewjkettle@gmail.com>
2020-06-27 13:47:52 -0700

    Values reading, filtering needed.

--- Embedded_Code/SSA/finalssa/imu/imu.h
+++ Embedded_Code/SSA/finalssa/imu/imu.h
@@ -1,27 +1,39 @@
 #ifndef _IMU_
 #define _IMU_
 
+//headers:
+#include "Wire.h"
 #include "Energia.h"
 
 //Functions:
 void I2C_init();
-float *getI2CData(float *imupointer);
+void getI2CData();
 int16_t convert_16bit(int8_t high, int8_t low);
 float accel_conversion(int16_t rawaccel);
 float gyro_conversion(int16_t rawgyro);
-void printData(float accelx, float accely, float accelz, float gyrox, float gyroy, float gyroz);
+void printData(float accelx, float accely, float accelz, float gyrox, float gyroy, float gryoz);
+
+/* variable declerations (globals)*/
+int count = 0;
+float overall_accelx, overall_accely, overall_accelz, overall_gyrox, overall_gyroy, overall_gyroz;
+
+//pin declerations
+int sda = P2_2; //I2C pins
+int scl = P2_1;
 
 //registers
 #define lsm9ds1_ag 0x6B //device reg
 
 #define gyro_control1 0x10 //Control of the gyro reg
 #define accel_control4 0x1E //Control of the accel reg4
-#define accel_control5 0x1F //Control of the accel reg5
 #define accel_control6 0x20 //Control of the accel reg6
-#define accel_control7 0x21 //Control of the accel reg5
+#define accel_control5 0x1F //Control of the accel reg5
+#define accel_control7 0x21 //Control of the accel reg7
 #define mag_pwr 0x16 //the register that powers the mangetometer
-#define strtw 0xD6 
+#define strtw 0xD6 //send before write?
 #define strtr 0xD5
+#define fifo_ctrl 0x2E
+#define fifo_src 0x2F
 
 #define gyroX0 0x18 //accel output registerss
 #define gyroX1 0x19
@@ -37,4 +49,4 @@ void printData(float accelx, float accely, float accelz, float gyrox, float gyro
 #define accelZ0 0x2C
 #define accelZ1 0x2D
 
-#endif 
+#endif

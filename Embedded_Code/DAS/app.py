bc590d1652bebbd0dfefc049c4af38170da57a2d
William Sun <sunwilliam@gmail.com>
2021-03-07 20:34:44 -0800

    Separated main route into smaller routes with redirects

--- Embedded_Code/DAS/app.py
+++ Embedded_Code/DAS/app.py
@@ -5,80 +5,73 @@
 
 import os
 from flask import Flask, render_template, url_for, request, redirect, flash
-# from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import io
 
 import daslib as das
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
 app.secret_key = "racecar"
 app.config["CSV_UPLOADS"] = UPLOAD_FOLDER
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# db = SQLAlchemy(app)
 
-'''
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-'''
 
-# used to make sure the user can only upload csv files
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
+	return render_template('index.html')
 
-    show_data = False
+@app.route('/file-list', methods=['GET'])
+def render_file_list():
+	return render_template('index.html')
 
-    #handle file upload
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # handle not uploading file 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        #handle invalid file type
-        if not allowed_file(file.filename):
-            flash("Invalid file type")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["CSV_UPLOADS"], file.filename))
-            sensor = request.form["sensor"]
-            func = request.form["function"]
-            (averages, absmins, absmaxes) = handle_data(filename, sensor, func)
-            show_data = True
-            '''
-            return redirect(url_for('handle_data',
-                                    filename=filename,
-                                    sensor=sensor,
-                                    function=func))'''
+@app.route('/file/upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
 
-    if show_data:
-        return render_template('index.html', filename = filename, sensor=sensor, function=func,
-        averages = averages, absmins = absmins, absmaxes = absmaxes, data= True)
-    else:
-        return render_template('index.html')
+	# handle not uploading file 
+	if file.filename == '':
+		flash('No selected file')
+		return redirect(request.url)
+
+	# get values from uploaded fields
+	filename = secure_filename(file.filename)
+	file.save(os.path.join(app.config["CSV_UPLOADS"], file.filename))
+	sensor = request.form["sensor"]
+	func = request.form["function"]
+
+	return redirect(url_for('view_uploaded_files', filename=filename, sensor=sensor, function=func))
+
+
+@app.route('/file/error', methods=['GET'])
+def file_error():
+	return render_template('file_error.html')
+
+
+@app.route('/file/<filename>/<sensor>/<function>', methods=['GET'])
+def view_uploaded_files(filename: str, sensor: str, function: str):
+	try:
+		(averages, absmins, absmaxes) = handle_data(filename, sensor, function)
+	except Exception:
+		return redirect('/file/error')
+
+	return render_template('data.html', filename=filename, sensor=sensor, function=function,
+		average=averages, absmins=absmins, absmaxes=absmaxes, data=True)
 
 
 def handle_data(filename,sensor,function):
-
     headernames = ["Accel_X", "Accel_Y", "Accel_Z", "Gyro_X", "Gyro_Y", "Gyro_Z", "IR_1", "IR_2", "IR_3", "Wheelspeed"]
 
     #read csv file, append headers
-    df = das.getdf("./uploads/" + filename, headernames) #gets dataframe from csv file
+    df = das.getdf(UPLOAD_FOLDER + filename, headernames) #gets dataframe from csv file
+
     #filter for desired sensor
     df = das.filterSensor(df, sensor)
 
@@ -121,13 +114,10 @@ def handle_data(filename,sensor,function):
 
         
         averages = das.get_avg(df)
-       
         absmins = das.get_absmin(df)
-        
         absmaxes = das.get_absmax(df)
 
         return (averages, absmins, absmaxes)
-    # plot = '/static/images/plot.png' previously used for static graph image
 
 
     
